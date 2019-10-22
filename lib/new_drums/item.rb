c080a2db66bd064395ed56c8343d27201f68e600
Jharell Chambers <jharell.chambers@selu.edu>
2019-10-22 01:27:12 +0000

    save

--- lib/new_drums/item.rb
+++ lib/new_drums/item.rb
@@ -3,21 +3,27 @@ class NewDrums::Item
  
   def self.today 
     
-    self.scrape_items
+   self.scrape_items
    end 
    
-   def self.scrape_items
-     items = []
-     
-     items << self.scrape_sweetwater
-    
-     
-     
-     return items
-  end 
   
-  def self.scrape_sweetwater
-    doc = Nokogiri::HTML(open("https://sweetwater.com"))
-    binding.pry
+  
+  def self.scrape_items
+    items = []
+    items << self.scrape_sweetwater
+    
+    return items
  end
+ 
+ 
+  def self.scrape_sweetwater
+    doc = Nokogiri::HTML(open("https://www.sweetwater.com/nowshipping/Drums"))
+    
+  
+    item = self.new 
+    item.name = doc.search("strong.product-block__title").text
+    item.price = doc.search("#grid em.product__price").text
+    
+    return item
+  end
 end 
