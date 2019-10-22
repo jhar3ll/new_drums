01dda10db859e9eddd97593c551ede8197ea543b
Jharell Chambers <jharell.chambers@selu.edu>
2019-10-22 00:12:20 +0000

    save5

--- lib/new_drums.rb
+++ lib/new_drums.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'nokogiri'
+
 require_relative "./new_drums/version"
 require_relative './new_drums/item.rb'
 require_relative './new_drums/cli.rb'
--- lib/new_drums/item.rb
+++ lib/new_drums/item.rb
@@ -2,17 +2,22 @@ class NewDrums::Item
  attr_accessor :name, :price, :url 
  
   def self.today 
-     item_1 = self.new 
-     item_1.name = "Tama boom stand 2-pack"
-     item_1.price = "$79.99"
-     item_1.url = "https://www.sweetwater.com/store/detail/HC03BWX2--tama-standard-boom-stand-2-pack"
-
-     item_2 = self.new 
-     item_2.name = "Mike Johnston groove bell"
-     item_2.price = "$49.99"
-     item_2.url = "https://www.sweetwater.com/store/detail/MJ-GB--meinl-percussion-mike-johnston-groove-bell-signature-cowbell"
-
-     [item_1, item_2]
+    
+    self.scrape_items
    end 
-
- end 
+   
+   def self.scrape_items
+     items = []
+     
+     items << self.scrape_sweetwater
+    
+     
+     
+     return items
+  end 
+  
+  def self.scrape_sweetwater
+    doc = Nokogiri::HTML(open("https://sweetwater.com"))
+    binding.pry
+ end
+end 
