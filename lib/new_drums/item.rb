9b8ca05244c76b8ed188e980b02a3e4d4c8c46a3
Jharell Chambers <jharell.chambers@selu.edu>
2019-10-23 03:15:07 +0000

    final save

--- lib/new_drums/item.rb
+++ lib/new_drums/item.rb
@@ -3,27 +3,21 @@ class NewDrums::Item
  
   def self.today 
     
-   self.scrape_items
+   self.scrape_popads
    end 
    
-  
-  
-  def self.scrape_items
-    items = []
-    items << self.scrape_popads
-    
-    return items
- end
- 
- 
   def self.scrape_popads
+    items = []
     doc = Nokogiri::HTML(open("https://www.popads.net/"))
-    
-    item = self.new
-    item.name = doc.search("div.dark.bold").first.text.strip
-    item.description = doc.search("div.spaced").first.text.strip
-    item.url = doc.search("a.dark").first.attr("href")
-   
-    return item
+    doc.search("div.blog_item").each do |blog|
+      
+      item = self.new
+      item.name = blog.search("div.dark").first.text.strip
+      item.description = blog.search("div.spaced").first.text.strip
+      item.url = blog.search("a.dark").first.attr("href")
+      
+      items << item 
+    end 
+    return items
   end
 end 
