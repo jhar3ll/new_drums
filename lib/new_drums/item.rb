7038e3ae037d086cd54d0f9584a202351f12131e
Jharell Chambers <jharell.chambers@selu.edu>
2019-10-23 02:02:36 +0000

    tues_save3

--- lib/new_drums/item.rb
+++ lib/new_drums/item.rb
@@ -10,19 +10,43 @@ class NewDrums::Item
   
   def self.scrape_items
     items = []
-    items << self.scrape_popads
+    items << self.scrape_popads1
+    items << scrape_popads2
+    items << scrape_popads3
+    
     
     return items
  end
  
  
-  def self.scrape_popads
+  def self.scrape_popads1
     doc = Nokogiri::HTML(open("https://www.popads.net/"))
     
-  
     item = self.new
     item.name = doc.search("div.dark.bold").first.text.strip
     item.description = doc.search("div.spaced").first.text.strip
+    item.url = doc.search("a.dark").first.attr("href")
+    
+    return item
+  end
+  
+  def self.scrape_popads2
+    doc = Nokogiri::HTML(open("https://www.popads.net/"))
+    
+    item = self.new
+    item.name = doc.search("div.dark.bold").text.strip
+    item.description = doc.search("div.spaced").text.strip
+    item.url = doc.search("a.dark").first.attr("href")
+    
+    return item
+  end
+  
+  def self.scrape_popads3
+    doc = Nokogiri::HTML(open("https://www.popads.net/"))
+    
+    item = self.new
+    item.name = doc.search("div.dark.bold").text.strip
+    item.description = doc.search("div.spaced").text.strip
     item.url = doc.search("a.dark").first.attr("href")
     
     return item
