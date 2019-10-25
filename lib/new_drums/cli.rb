72012d2d85f7599c487f9a56a0c6912e9edd6c78
Jharell Chambers <jharell.chambers@selu.edu>
2019-10-25 22:18:43 +0000

    final_save_1025

--- lib/new_drums/cli.rb
+++ lib/new_drums/cli.rb
@@ -19,7 +19,7 @@ class NewDrums::CLI
        puts "Enter the number of the blog you'd like more info on, type \"list\" for the list of blogs or type \"exit\" to exit."
        input = gets.strip.downcase
        
-       if input.to_i > 0  
+       if input.to_i > 0 && input.to_i < 4  
          the_item = @items[input.to_i - 1]
         puts "#{the_item.name}\n\n#{the_item.description} \n\nurl: (#{the_item.url})\n\n"
       elsif input == "list"
